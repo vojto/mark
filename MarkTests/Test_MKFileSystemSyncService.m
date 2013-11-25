0dfc5f8c04848138e70cd99255384c262517b0e4
Vojtech Rinik <vojto@rinik.net>
2013-11-25 16:05:09 +0100

    Adding test for creating files for notes

--- MarkTests/Test_MKFileSystemSyncService.m
+++ MarkTests/Test_MKFileSystemSyncService.m
@@ -11,12 +11,14 @@ describe(@"MKFileSystemSyncService", ^{
     __block MKFileSystemSyncService *service;
     __block NSManagedObjectContext *context;
     __block NSUserDefaults *defaults;
+    __block NSFileManager *manager;
     
     beforeAll(^{
         [MagicalRecord setDefaultModelFromClass:[self class]];
         [MagicalRecord setupCoreDataStackWithInMemoryStore];
         context = [NSManagedObjectContext defaultContext];
         defaults = [NSUserDefaults standardUserDefaults];
+        manager = [NSFileManager defaultManager];
     });
     
     it(@"loads base path from defaults", ^{
@@ -30,6 +32,30 @@ describe(@"MKFileSystemSyncService", ^{
         service = [[MKFileSystemSyncService alloc] initWithContext:context];
         [defaults setObject:@"test2" forKey:@"filesystemPath"];
         expect(service.basePath).to.equal(@"test2");
+        service = nil;
+    });
+    
+    describe(@"syncing with the file system", ^{
+        fit(@"creates file for the note", ^{
+            NSString *basePath = @"/tmp/MARK_NOTES_TEST";
+            [defaults setObject:basePath forKey:@"filesystemPath"];
+            service = [[MKFileSystemSyncService alloc] initWithContext:context];
+            
+            // Set up test path
+            [manager createDirectoryAtPath:basePath withIntermediateDirectories:YES attributes:nil error:NULL];
+            
+            // Create a test note
+            MKNote *note = [MKNote createEntity];
+            note.title = @"foo";
+            note.content = @"foo bar";
+            [context save:NULL];
+            
+            NSString *path = [basePath stringByAppendingPathComponent:@"foo.md"];
+            expect([manager fileExistsAtPath:path]).to.beTruthy();
+
+            
+            
+        });
     });
 });
 
