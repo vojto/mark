9de1bc50ee3262d7209a5ace80abd6ed6b0b8d01
Vojtech Rinik <vojto@rinik.net>
2013-11-25 16:57:21 +0100

    Adding failing test for restoring from the filesystem

--- MarkTests/Test_MKFileSystemSyncService.m
+++ MarkTests/Test_MKFileSystemSyncService.m
@@ -50,6 +50,9 @@ describe(@"MKFileSystemSyncService", ^{
             while(file = [enumerator nextObject]) {
                 [manager removeItemAtPath:[basePath stringByAppendingPathComponent:file] error:NULL];
             }
+            for (MKNote *note in [MKNote findAll]) {
+                [note deleteEntity];
+            }
         });
         
         it(@"creates file for the note", ^{
@@ -64,11 +67,37 @@ describe(@"MKFileSystemSyncService", ^{
             expect([manager fileExistsAtPath:path]).to.beTruthy();
         });
         
-        fit(@"restores note from file", ^{
+        it(@"restores note from file", ^{
             NSString *content = @"a note\n\n<!-- Mark: xxx1|tag1 -->";
             [content writeToFile:[basePath stringByAppendingPathComponent:@"restore.md"] atomically:YES encoding:NSUTF8StringEncoding error:NULL];
             [service restoreFromFileSystem];
             usleep(300*1000); // wait 0.1s for restoring to finish
+            NSArray *notes = [MKNote findAll];
+            expect(notes.count).to.equal(1);
+        });
+        
+        fit(@"removes file that was removed from disk", ^{
+            // First, create 2 notes and sync them to the disk
+            MKNote *note1 = [MKNote createEntity];
+            note1.title = @"note1";
+            MKNote *note2 = [MKNote createEntity];
+            note2.title = @"note2";
+            [context saveToPersistentStoreAndWait];
+            usleep(300*1000);
+            
+            // Make sure the files were created
+            NSString *path = [basePath stringByAppendingPathComponent:@"note1.md"];
+            expect([manager fileExistsAtPath:path]).to.beTruthy();
+            path = [basePath stringByAppendingPathComponent:@"note2.md"];
+            expect([manager fileExistsAtPath:path]).to.beTruthy();
+
+            // Remove one of the files
+            [manager removeItemAtPath:[basePath stringByAppendingPathComponent:@"note1.md"] error:NULL];
+            
+            // Restore from filesystem
+            [service restoreFromFileSystem];
+            
+            // Assert that it was removed from Core Data storage
             NSArray *notes = [MKNote findAll];
             expect(notes.count).to.equal(1);
         });
