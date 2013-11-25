05238dd50f1ace7fcea7d719ab8df33603e4a646
Vojtech Rinik <vojto@rinik.net>
2013-11-25 16:39:51 +0100

    Testing restoring from filesystem

--- MarkTests/Test_MKFileSystemSyncService.m
+++ MarkTests/Test_MKFileSystemSyncService.m
@@ -40,9 +40,16 @@ describe(@"MKFileSystemSyncService", ^{
         beforeAll(^{
             basePath = @"/tmp/MARK_NOTES_TEST";
             [manager createDirectoryAtPath:basePath withIntermediateDirectories:YES attributes:nil error:NULL];
-            
             service = [[MKFileSystemSyncService alloc] initWithContext:context];
             [defaults setObject:basePath forKey:@"filesystemPath"];
+        });
+        
+        beforeEach(^{
+            NSDirectoryEnumerator *enumerator = [manager enumeratorAtPath:basePath];
+            NSString *file;
+            while(file = [enumerator nextObject]) {
+                [manager removeItemAtPath:[basePath stringByAppendingPathComponent:file] error:NULL];
+            }
         });
         
         it(@"creates file for the note", ^{
@@ -52,9 +59,18 @@ describe(@"MKFileSystemSyncService", ^{
             note.content = @"foo bar";
             NSLog(@"Context: %@", context);
             [context save:NULL];
-            
+            usleep(100*1000); // wait 0.1s for saving to finish
             NSString *path = [basePath stringByAppendingPathComponent:@"foo.md"];
             expect([manager fileExistsAtPath:path]).to.beTruthy();
+        });
+        
+        fit(@"restores note from file", ^{
+            NSString *content = @"a note\n\n<!-- Mark: xxx1|tag1 -->";
+            [content writeToFile:[basePath stringByAppendingPathComponent:@"restore.md"] atomically:YES encoding:NSUTF8StringEncoding error:NULL];
+            [service restoreFromFileSystem];
+            usleep(300*1000); // wait 0.1s for restoring to finish
+            NSArray *notes = [MKNote findAll];
+            expect(notes.count).to.equal(1);
         });
         
         afterAll(^{
